b980af95de1fd04256a16eb37e25dcd100533de8
Matteo Zanatta <matteo.zanatta99@icloud.com>
2022-09-08 00:01:32 +0200

    added new animations on rockets.html page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,31 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('pageLoad', [
+      state('initial', style({ transform: 'scale(0)' })),
+      state('loading', style({ transform: 'scale(1)' })),
+      transition('initial => loading', animate('400ms 1ms ease-in-out')),
+    ]),
+  ],
 })
 export class HeaderComponent implements OnInit {
+  animationLoading: String = 'initial';
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.animationLoading = 'loading';
+  }
 }
